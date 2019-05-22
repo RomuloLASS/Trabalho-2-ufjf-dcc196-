21ad9be438ee5671659eb84e5a30a76b843cf63c
Romulo Soares <37163123+RomuloLASS@users.noreply.github.com>
2019-05-22 00:49:47 -0300

    Dados do banco sendo mostrados no RecyclerView e acesso a DisciplinasActivity feito

--- app/src/main/java/br/ufjf/dcc196/trabalho01/NovoPlanejamentoActivity.java
+++ app/src/main/java/br/ufjf/dcc196/trabalho01/NovoPlanejamentoActivity.java
@@ -1,7 +1,9 @@
 package br.ufjf.dcc196.trabalho01;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,34 +29,32 @@ public class NovoPlanejamentoActivity extends AppCompatActivity {
         btnCadastraPlanejamento.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText etAno = findViewById(R.id.etAno);
-                EditText etLinguas = findViewById(R.id.etLingua);
-                EditText etHumanas = findViewById(R.id.etHumanas);
-                EditText etExatas = findViewById(R.id.etExatas);
-                EditText etSaude = findViewById(R.id.etSaude);
-                EditText etSemestre = findViewById((R.id.etSemestre));
+        PlanejamentosDBHelper helper = new PlanejamentosDBHelper(getApplicationContext());
+        SQLiteDatabase db = helper.getWritableDatabase();
 
-                int count = Integer.valueOf(etLinguas.getText().toString())+Integer.valueOf(etHumanas.getText().toString())+Integer.valueOf(etExatas.getText().toString())+Integer.valueOf(etSaude.getText().toString());
+        EditText etAno = findViewById(R.id.etAno);
+        EditText etLinguas = findViewById(R.id.etLingua);
+        EditText etHumanas = findViewById(R.id.etHumanas);
+        EditText etExatas = findViewById(R.id.etExatas);
+        EditText etSaude = findViewById(R.id.etSaude);
+        EditText etSemestre = findViewById((R.id.etSemestre));
 
-                /*Planejamentos planejamento = new Planejamentos();
+        int countHoras = Integer.valueOf(etLinguas.getText().toString())+Integer.valueOf(etHumanas.getText().toString())+Integer.valueOf(etExatas.getText().toString())+Integer.valueOf(etSaude.getText().toString());
 
-                planejamento.setAno(Integer.valueOf(String.valueOf(etAno.getText())));
-                planejamento.setSemestre(Integer.valueOf(String.valueOf(etSemestre.getText())));
-                planejamento.setHorasLinguas(Float.valueOf(etLinguas.getText().toString()));
-                planejamento.setHorasHumanas(Float.valueOf(etHumanas.getText().toString()));
-                planejamento.setHorasExatas(Float.valueOf(etExatas.getText().toString()));
-                planejamento.setHorasSaude(Float.valueOf(etSaude.getText().toString()));
 
-                Intent intent = new Intent();
-                intent.putExtra("ano", planejamento.getAno());
-                intent.putExtra("semestre", planejamento.getSemestre());
-                intent.putExtra("hLinguas", planejamento.getHorasLinguas());
-                intent.putExtra("hHumanas", planejamento.getHorasHumanas());
-                intent.putExtra("hExatas", planejamento.getHorasExatas());
-                intent.putExtra("hSaude", planejamento.getHorasHumanas());
-                */
-                //setResult(Activity.RESULT_OK, intent);
-                finish();
+        ContentValues values =  new ContentValues();
+        values.put(PlanejamentosContract.Planejamentos.COLLUMN_ANO, String.valueOf(etAno.getText()));
+        values.put(PlanejamentosContract.Planejamentos.COLLUMN_SEMESTRE, String.valueOf(etSemestre.getText()));
+        values.put(PlanejamentosContract.Planejamentos.COLLUMN_HORAS, String.valueOf(countHoras));
+        values.put(PlanejamentosContract.Planejamentos.COLLUMN_LINGUAS, String.valueOf(etLinguas.getText()));
+        values.put(PlanejamentosContract.Planejamentos.COLLUMN_HUMANAS, String.valueOf(etHumanas.getText()));
+        values.put(PlanejamentosContract.Planejamentos.COLLUMN_EXATAS, String.valueOf(etExatas.getText()));
+        values.put(PlanejamentosContract.Planejamentos.COLLUMN_SAUDE, String.valueOf(etSaude.getText()));
+
+        db.insert(PlanejamentosContract.Planejamentos.TABLE_NAME, null, values);
+
+        setResult(Activity.RESULT_OK);
+        finish();
             }
         });
     }
--- app/src/main/java/br/ufjf/dcc196/trabalho01/PlanejamentosActivity.java
+++ app/src/main/java/br/ufjf/dcc196/trabalho01/PlanejamentosActivity.java
@@ -22,6 +22,7 @@
     public class PlanejamentosActivity extends AppCompatActivity {
         public static final int REQUEST_PLANEJAMENTO = 1;
         public static final int REQUEST_MATERIA = 2;
+        Cursor cursorPlanrjamento;
 
         //ArrayList<Planejamentos> planejamentos = new ArrayList<>();
 
@@ -42,18 +43,6 @@
 
             PlanejamentosDBHelper helper = new PlanejamentosDBHelper(getApplicationContext());
             SQLiteDatabase db = helper.getWritableDatabase();
-            ContentValues values =  new ContentValues();
-            values.put(PlanejamentosContract.Planejamentos.COLLUMN_ANO, "2016");
-            values.put(PlanejamentosContract.Planejamentos.COLLUMN_SEMESTRE, "1");
-            values.put(PlanejamentosContract.Planejamentos.COLLUMN_HORAS, "20");
-            values.put(PlanejamentosContract.Planejamentos.COLLUMN_LINGUAS, "5");
-            values.put(PlanejamentosContract.Planejamentos.COLLUMN_HUMANAS, "5");
-            values.put(PlanejamentosContract.Planejamentos.COLLUMN_EXATAS, "5");
-            values.put(PlanejamentosContract.Planejamentos.COLLUMN_SAUDE, "5");
-
-            long id = db.insert(PlanejamentosContract.Planejamentos.TABLE_NAME, null, values);
-
-            Toast.makeText(this, "Novo registro criado com id: " + id, Toast.LENGTH_LONG).show();
 
             String[] camposPlanejamento = {
                     PlanejamentosContract.Planejamentos._ID,
@@ -66,23 +55,20 @@
                     PlanejamentosContract.Planejamentos.COLLUMN_SAUDE,
             };
 
-            Cursor cursorPlanrjamento = db.query(PlanejamentosContract.Planejamentos.TABLE_NAME, camposPlanejamento, null, null, null, null, null);
+            cursorPlanrjamento = db.query(PlanejamentosContract.Planejamentos.TABLE_NAME, camposPlanejamento, null, null, null, null, null);
 
             final PlanejamentoAdapter adapter = new PlanejamentoAdapter(cursorPlanrjamento);
             RecyclerView rv = findViewById(R.id.rvPlanejamentos);
             rv.setAdapter(adapter);
             rv.setLayoutManager(new LinearLayoutManager(this));
 
-            /*
-            pAdapter.setListener(new PlanejamentosAdapter.OnPlanejamentoClickListener() {
+            adapter.setOnPlanejamentoClickListener(new PlanejamentoAdapter.OnPlanejamentoClickListener() {
                 @Override
-                public void onPlanejamentoClick(View v, int position) {
+                public void onPlanejamentoClick(View livroView, int position) {
                     Intent intent = new Intent(PlanejamentosActivity.this, DisciplinasCursadasActivity.class);
-                    intent.putExtra("dados", planejamentos.get(position).getDisciplinas());
                     startActivity(intent);
                 }
             });
-            */
 
             btnPlanejamento.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -99,42 +85,5 @@
                     startActivityForResult(intent, PlanejamentosActivity.REQUEST_MATERIA);
                 }
             });
-
         }
-
-        /*
-        @Override
-        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-            if (data != null) {
-                if (requestCode == PlanejamentosActivity.REQUEST_PLANEJAMENTO && resultCode == PlanejamentosActivity.RESULT_OK) {
-                    Bundle bundle = data.getExtras();
-                    Planejamentos t = new Planejamentos();
-                    t.setAno((Integer) bundle.get("ano"));
-                    t.setSemestre((Integer) bundle.get("semestre"));
-                    t.setHorasLinguas((Float) bundle.get("hLinguas"));
-                    t.setHorasHumanas((Float) bundle.get("hHumanas"));
-                    t.setHorasExatas((Float) bundle.get("hExatas"));
-                    t.setHorasSaude((Float) bundle.get("hSaude"));
-                    t.calculaHoras();
-                    this.planejamentos.add(t);
-                }
-                else if (requestCode == PlanejamentosActivity.REQUEST_MATERIA && resultCode == PlanejamentosActivity.RESULT_OK){
-                    Bundle bundle = data.getExtras();
-                    Disciplinas tempD = new Disciplinas();
-                    int ano, semestre;
-                    tempD.setNome((String) bundle.get("nome"));
-                    tempD.setThoras((Float) bundle.get("tHora"));
-                    tempD.setArea((String) bundle.get("area"));
-
-                    ano = (int) bundle.get("ano");
-                    semestre = (int) bundle.get("semestre");
-
-                    for (int i=0; i<planejamentos.size(); i++){
-                        if(planejamentos.get(i).getAno() == ano && planejamentos.get(i).getSemestre() == semestre){
-                            planejamentos.get(i).setDisciplinas(tempD);
-                        }
-                    }
-                }
-            }
-        }*/
     }
