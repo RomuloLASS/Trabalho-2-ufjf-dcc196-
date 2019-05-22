6bdf1f17439f0a9e242500c30b62b68fa318ad75
Romulo Soares <37163123+RomuloLASS@users.noreply.github.com>
2019-05-21 23:06:04 -0300

    PlanejamentoContract finalizada

--- app/src/main/java/br/ufjf/dcc196/trabalho01/PlanejamentosContract.java
+++ app/src/main/java/br/ufjf/dcc196/trabalho01/PlanejamentosContract.java
@@ -5,15 +5,18 @@ import android.provider.BaseColumns;
 public class PlanejamentosContract {
     public static class Planejamentos implements BaseColumns{
         //Colunas da tabela
-        public static final String TABLE_ANO = "ano";
+        public static final String TABLE_NAME = "planejamento";
+        public static final String COLLUMN_ANO = "ano";
         public static final String COLLUMN_SEMESTRE = "semestre";
         public static final String COLLUMN_HORAS = "horas";
         public static final String COLLUMN_LINGUAS = "linguas";
         public static final String COLLUMN_HUMANAS = "humanas";
         public static final String COLLUMN_EXATAS = "exatas";
         public static final String COLLUMN_SAUDE = "saude";
-
-
+        //SQL para a criação da tabela
+        public static final String CREATE_TABLE = String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT , %s TEXT, %s TEXT, %s INTEGER)", TABLE_NAME, _ID, COLLUMN_ANO, COLLUMN_SEMESTRE, COLLUMN_HORAS, COLLUMN_LINGUAS, COLLUMN_HUMANAS, COLLUMN_EXATAS, COLLUMN_SAUDE);
+        //SQL para deletar a tabela
+        public static final String DROP_TABLE = String.format("DROP TABLE %s", TABLE_NAME);
     }
 
 }
